b3d48906d2e2ab0b7b692a5594dba6ea5b16b68b
lauraferrandof <lauraferrandof@gmail.com>
2019-01-14 15:36:12 +0100

    Fix constructor in App.js

--- src/App.js
+++ src/App.js
@@ -85,11 +85,13 @@ const pokemon = [
 ];
 
 class App extends Component {
-  /* constructor(props) {
+  constructor(props) {
+    super(props);
     this.pokemon = pokemon;
-  } */
+  }
+
   render() {
-    //const { pokemon } = this;
+    const { pokemon } = this;
     return (
       <div className="App">
         <h1>Mi lista de pokemon</h1>
