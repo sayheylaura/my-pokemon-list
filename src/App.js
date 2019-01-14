dcfd6ceaa4404671dc03fa5d31b00d023bcf5a5d
lauraferrandof <lauraferrandof@gmail.com>
2019-01-14 15:34:34 +0100

    Create item list with map in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,113 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const pokemon = [
+  {
+    id: 1,
+    name: "bulbasaur",
+    types: ["poison", "grass"],
+    evolution: null,
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+  },
+  {
+    id: 2,
+    name: "ivysaur",
+    types: ["poison", "grass"],
+    evolution: "bulbasaur",
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
+  },
+  {
+    id: 3,
+    name: "venusaur",
+    types: ["poison", "grass"],
+    evolution: "ivysaur",
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
+  },
+  {
+    id: 4,
+    name: "charmander",
+    types: ["fire"],
+    evolution: null,
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+  },
+  {
+    id: 5,
+    name: "charmeleon",
+    types: ["fire"],
+    evolution: "charmander",
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
+  },
+  {
+    id: 6,
+    name: "charizard",
+    types: ["flying", "fire"],
+    evolution: "charmeleon",
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
+  },
+  {
+    id: 7,
+    name: "squirtle",
+    types: ["water"],
+    evolution: null,
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+  },
+  {
+    id: 8,
+    name: "wartortle",
+    types: ["water"],
+    evolution: "squirtle",
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png"
+  },
+  {
+    id: 9,
+    name: "blastoise",
+    types: ["water"],
+    evolution: "wartortle",
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png"
+  },
+  {
+    id: 10,
+    name: "caterpie",
+    types: ["bug"],
+    evolution: null,
+    url:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"
+  }
+];
 
 class App extends Component {
+  /* constructor(props) {
+    this.pokemon = pokemon;
+  } */
   render() {
+    //const { pokemon } = this;
     return (
       <div className="App">
-
+        <h1>Mi lista de pokemon</h1>
+        <ul>
+          {pokemon.map(item => {
+            return (
+              <li key={item.id}>
+                <img src={item.url} alt="" />
+                <h2>{item.name}</h2>
+                <ul>
+                  {item.types.map((type, i) => {
+                    return <li key={i}>{type}</li>;
+                  })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
