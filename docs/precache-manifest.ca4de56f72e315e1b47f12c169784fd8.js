550494cb153e31d3d0b0ebbc357d4a300277814b
lauraferrandof <lauraferrandof@gmail.com>
2019-01-14 16:16:13 +0100

    Create docs folder for production

---
+++ docs/precache-manifest.ca4de56f72e315e1b47f12c169784fd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7bc604e595d85b5e0140",
+    "url": "./static/js/main.7bc604e5.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "7bc604e595d85b5e0140",
+    "url": "./static/css/main.a9e71459.chunk.css"
+  },
+  {
+    "revision": "39ce6d9291942c8684ae04f14b5e08da",
+    "url": "./index.html"
+  }
+];
